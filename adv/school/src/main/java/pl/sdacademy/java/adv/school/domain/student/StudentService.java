8d611539254328400cb20ad97b655791d7aebde4
Krzysztof <krzysztofbialy@o2.pl>
2022-01-12 21:13:41 +0100

    getStudentsMappedByIdentifier

--- adv/school/src/main/java/pl/sdacademy/java/adv/school/domain/student/StudentService.java
+++ adv/school/src/main/java/pl/sdacademy/java/adv/school/domain/student/StudentService.java
@@ -4,6 +4,8 @@ import pl.sdacademy.java.adv.school.domain.student.model.Student;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class StudentService {
@@ -22,5 +24,38 @@ public class StudentService {
                         .thenComparing(Student::getLastName)
                         .thenComparing(Student::getFirstName))
                 .collect(Collectors.toUnmodifiableList());
+    }
+
+    public List<Student> getStudentsSortedByAge() {
+
+        List<Student> allStudentsAge = studentRepository.findAllStudents();
+
+        return allStudentsAge.stream()
+                .sorted(Comparator.comparing(Student::getBirthDate))
+                .collect(Collectors.toUnmodifiableList());
+
+    }
+
+    public List<Student> getStudentsSortedByAgeDesc() {
+        List<Student> allStudentsAge = studentRepository.findAllStudents();
+
+        return allStudentsAge.stream()
+                .sorted(Comparator.comparing(Student::getBirthDate).reversed())
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    public Map<String, List<Student>> getStudentsGroupedByCityAndSortedByName() {
+
+        Map<String, List<Student>> groupedStudents =
+                studentRepository.findAllStudents().stream()
+                        .sorted(Comparator.comparing(Student::getLastName).thenComparing(Student::getFirstName))
+                        .collect(Collectors.groupingBy(Student::getCity));
+        return groupedStudents;
+    }
+
+    public Map<String, Student> getStudentsMappedByIdentifier() {
+        return studentRepository.findAllStudents().stream()
+        //        .collect(Collectors.toMap(Student::getId, student -> student))
+                .collect(Collectors.toMap(Student::getId, Function.identity()));
     }
 }
